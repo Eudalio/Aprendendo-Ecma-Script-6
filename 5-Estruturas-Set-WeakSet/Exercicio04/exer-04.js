5b6a1bc635679c14804c783bb56b256f61e60c3e
eudalio <tonnydallios@gmail.com>
2018-02-06 19:31:24 -0200

    Adicionado Exercicio 04

---
+++ 5-Estruturas-Set-WeakSet/Exercicio04/exer-04.js
@@ -0,0 +1,3 @@
+console.log("Cite um caso onde podemos utilizar a estrutura de WeakMap.");
+
+console.log("R: Um dos casos de uso mais interessantes é o de garantir que certo método ou propriedade pertence a um objeto específico e não a todas as instâncias do mesmo tipo. Mas no uso geral, sempre que você tiver preocupação com vazamento de memória, o WeakSet estará a seu dispor.");
